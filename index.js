2ad137932430817347bcac752e3784d8ec197d40
Farah alsoqi <farahmanna97@gmail.com>
2023-09-27 22:09:17 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+let names = ["Charlie", "Samip", "Ali"];
+let eventName = "surprise";
+
+wrapGifts(names);
+function writeCards(names, eventName) { 
+
+    let messages = [];
+    for (let i = 0; i < names.length; i++) {
+      let messagename = `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`;
+      messages.push(messagename);
+    }
+    return messages;
+  }
+  
+  function countDown(){
+    let z =10;
+    while (z>=0)
+    {
+        console.log(z)
+        z--
+    }
+  }
